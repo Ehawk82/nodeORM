be047cef3fed1bf6896555e3d0c1831b64055fee
Jason Graziano <mrehawk82@gmail.com>
2020-03-25 17:22:10 -0400

    ...

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function nodeorm(x) { return x };
+function nodeorm(x) { 
+	var z;
+	if (x === 0) { z = "VERY LIKLEY" };
+	if (x === 1) { z = "LIKELY" };
+	if (x === 2) { z = "EVEN" };
+	if (x === 3) { z = "UNLIKELY" };
+	if (x === 4) { z = "VERY UNLIKELY" };
+	return z
+};
 
 module.exports = { nodeorm: nodeorm };
